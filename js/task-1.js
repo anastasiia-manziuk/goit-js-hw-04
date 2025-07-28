d65edae1f077d6bf0c3f5cb9876becfad12489cb
ANASTASIIA <nastamanzuk62@gmail.com>
2025-07-28 15:05:03 +0200

    add 3 tasks in hw-4

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,26 @@
-function slugify(title) {   
-const slug = title.toLowerCase().split(" ").join("-");        
-return slug;
+function isEnoughCapacity(products, containerSize) {
+    let total = 0; 
+
+    for (let fruit in products) { 
+    total += products[fruit];
+    }
+
+    return total <= containerSize;
 }
 
-console.log(slugify("Arrays for beginners"));
-console.log(slugify("English for developer"));
-console.log(slugify("Ten secrets of JavaScript"));
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+
+console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); 
+
+console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); 
+
+console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); 
+
+console.log(
+    isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+);
